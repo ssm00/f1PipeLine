7b0e11c6b1a187493c856b5a1ab8218a647015e9
ssm00 <titan0724@mju.ac.kr>
2024-06-28 21:51:50 -0600

    Update TitleImageGenerator.py

--- ImageModifier/TitleImageGenerator.py
+++ ImageModifier/TitleImageGenerator.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 
 size=(1080,1350)
-base_pic=np.zeros((size[1],size[0],3),np.uint8)
+base_pic=cv2.imread("../after_processing_image/background.jpg")
 pic1 = cv2.imread("../download_image/MONTE-CARLO,_MONACO_-_MAY_24__Kevin_Magnussen_of_Denmark_driving_the_(20)_Haas_F1_VF-24_Ferrari_in.jpg")
 h,w=pic1.shape[:2]
 ash=size[1]/h
@@ -13,15 +13,9 @@ if asw<ash:
 else:
     sizeas=(int(w*ash),int(h*ash))
 pic1 = cv2.resize(pic1,dsize=sizeas)
-base_pic[int(size[1]/2-sizeas[1]/2):int(size[1]/2+sizeas[1]/2), int(size[0]/2-sizeas[0]/2):int(size[0]/2+sizeas[0]/2),:]=pic1
+#base_pic[int(size[1]/2-sizeas[1]/2):int(size[1]/2+sizeas[1]/2), int(size[0]/2-sizeas[0]/2):int(size[0]/2+sizeas[0]/2),:]=pic1
+base_pic[0:sizeas[1], 0:sizeas[0],:]=pic1
 cv2.imshow("pc1",base_pic)
 os.chdir("../after_processing_image")
-cv2.imwrite("AA.jpg",base_pic)
+cv2.imwrite("../after_processing_image/"+"pic1"+".jpg",base_pic)
 
-
-
-# resize1 = cv2.resize(img, (1080, 1350))
-# resize2 = cv2.resize(img, (0, 0), fx=0.3, fy=0.8, interpolation=cv2.INTER_AREA)
-# cv2.imshow("resize1", resize1)
-# cv2.imshow("resize2", resize2)
-# cv2.waitKey(0)
