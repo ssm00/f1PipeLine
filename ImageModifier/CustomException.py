3040fc94d2fd4045b46b0569d53eaf65468d99c3
ssm00 <titan0724@mju.ac.kr>
2024-07-06 22:40:52 -0600

    Update CustomException.py

--- ImageModifier/CustomException.py
+++ ImageModifier/CustomException.py
@@ -1,3 +1,7 @@
 class TooMuchLogin(Exception):
     def __init__(self):
         self.msg = "too much login"
+
+class OutOfTextBox(Exception):
+    def __init__(self, size):
+        self.size = size
