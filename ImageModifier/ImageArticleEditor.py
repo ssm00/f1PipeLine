5d4bf0249c396bff25c3b2566e97e913453df80a
ssm00 <titan0724@mju.ac.kr>
2024-07-13 14:05:58 -0600

    Update ImageArticleEditor.py

--- ImageModifier/ImageArticleEditor.py
+++ ImageModifier/ImageArticleEditor.py
@@ -61,7 +61,7 @@ def add_icon_to_image(image_path, icon_path, position):
 
 # article_type은
 # Information, Breaking, Official, Tech, Rumor 다섯가지
-def add_basic_icon_to_image(image_path, article_type):
+def add_title_icon_to_image(image_path, article_type):
     image_name = os.path.splitext(os.path.basename(image_path))[0]
     if article_type == "Information":
         icon_path = '../prefab/information_icon.png'
@@ -89,8 +89,10 @@ def add_basic_icon_to_image(image_path, article_type):
     output_path = prefix_after_processing_path + image_name + ".png"
     cv2.imwrite(output_path, image_with_line)
 
-
-def resize_image(image_path):
+def resize_image_type1(image_path):
+    """
+    1080, 1350 resize
+    """
     image_name = os.path.splitext(os.path.basename(image_path))[0]
     image = cv2.imread(image_path, cv2.IMREAD_COLOR)
     h, w = image.shape[:2]
@@ -125,21 +127,105 @@ def resize_image(image_path):
     cv2.imwrite(prefix_after_processing_path + image_name + ".png", resized_cropped_image)
     return resized_cropped_image
 
+# def apply_alpha_gradient_to_image(image_path):
+#     """
+#         이미지의 특정 높이 이후로 선명도를 줄여 검정색으로 변환합니다.
+#
+#         Parameters:
+#         - image: 처리할 1080x1350 이미지
+#
+#         Returns:
+#         - 그라데이션 처리가 된 1080x1350 이미지
+#         """
+#     image_name = os.path.splitext(os.path.basename(image_path))[0]
+#     image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+#
+#     h, w = image.shape[:2]
+#     assert h == 1350 and w == 1080, "입력 이미지 크기는 1080x1350이어야 합니다."
+#
+#     # 구간 설정
+#     first_start_height = 700
+#     first_end_height = 800
+#     second_start_height = 800
+#     second_end_height = 1150
+#     third_start_height = 1150
+#     third_end_height = 1250
+#     fourth_start_height = 1250
+#     fourth_end_height = 1350
+#
+#     first_gradient_height = first_end_height - first_start_height
+#     second_gradient_height = second_end_height - second_start_height
+#     third_gradient_height = third_end_height - third_start_height
+#     fourth_gradient_height = fourth_end_height - fourth_start_height
+#
+#     # 첫 번째 그라데이션 알파 값
+#     alpha1 = np.linspace(1, 0.3, first_gradient_height).reshape(-1, 1)
+#     alpha1 = np.repeat(alpha1, w, axis=1)
+#     alpha1 = np.stack([alpha1] * 3, axis=2)  # RGB 채널 적용
+#
+#     # 두 번째 그라데이션 알파 값
+#     alpha2 = np.linspace(0.3, 0.2, second_gradient_height).reshape(-1, 1)
+#     alpha2 = np.repeat(alpha2, w, axis=1)
+#     alpha2 = np.stack([alpha2] * 3, axis=2)  # RGB 채널 적용
+#
+#     # 세 번째 그라데이션 알파 값
+#     alpha3 = np.linspace(0.2, 0.1, third_gradient_height).reshape(-1, 1)
+#     alpha3 = np.repeat(alpha3, w, axis=1)
+#     alpha3 = np.stack([alpha3] * 3, axis=2)  # RGB 채널 적용
+#
+#     # 네 번째 구간 알파 값
+#     alpha4 = np.linspace(0.1, 0, third_gradient_height).reshape(-1, 1)
+#     alpha4 = np.repeat(alpha4, w, axis=1)
+#     alpha4 = np.stack([alpha4] * 3, axis=2)  # RGB 채널 적용
+#
+#     # 검정색 배경 생성
+#     black_background = np.zeros((h, w, 3), dtype=np.uint8)
+#
+#     result_image = image.copy()
+#
+#     # 원본 이미지의 첫 번째 하단 부분을 알파 블렌딩
+#     result_image[first_start_height:first_end_height] = (
+#             image[first_start_height:first_end_height] * alpha1 + black_background[
+#                                                                   first_start_height:first_end_height] * (1 - alpha1)
+#     ).astype(np.uint8)
+#
+#     # 원본 이미지의 두 번째 하단 부분을 알파 블렌딩
+#     result_image[second_start_height:second_end_height] = (
+#             image[second_start_height:second_end_height] * alpha2 + black_background[
+#                                                                     second_start_height:second_end_height] * (
+#                     1 - alpha2)
+#     ).astype(np.uint8)
+#
+#     # 원본 이미지의 세 번째 하단 부분을 알파 블렌딩
+#     result_image[third_start_height:third_end_height] = (
+#             image[third_start_height:third_end_height] * alpha3 + black_background[
+#                                                                   third_start_height:third_end_height] * (1 - alpha3)
+#     ).astype(np.uint8)
+#
+#     # 원본 이미지의 네 번째 하단 부분을 알파 블렌딩 (0으로 유지)
+#     result_image[fourth_start_height:fourth_end_height] = (
+#             image[fourth_start_height:fourth_end_height] * alpha4 + black_background[
+#                                                                     fourth_start_height:fourth_end_height] * (
+#                     1 - alpha4)
+#     ).astype(np.uint8)
+#     # 결과 저장
+#     cv2.imwrite(prefix_after_processing_path + image_name + ".png", result_image)
+#     return result_image
 
-def apply_alpha_gradient_to_image(image_path):
+def apply_alpha_gradient_to_image_type1(image_path):
     """
-        이미지의 특정 높이 이후로 선명도를 줄여 검정색으로 변환합니다.
+    이미지의 특정 높이 이후로 선명도를 줄여 검정색으로 변환.
 
-        Parameters:
-        - image: 처리할 1080x1350 이미지
+    Parameters:
+    - image: 처리할 1080x1350 이미지
 
-        Returns:
-        - 그라데이션 처리가 된 1080x1350 이미지
-        """
+    Returns:
+    - 그라데이션 처리가 된 1080x1350 이미지
+    """
     image_name = os.path.splitext(os.path.basename(image_path))[0]
-    image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+    image = Image.open(image_path).convert("RGB")
 
-    h, w = image.shape[:2]
+    w, h = image.size
     assert h == 1350 and w == 1080, "입력 이미지 크기는 1080x1350이어야 합니다."
 
     # 구간 설정
@@ -157,60 +243,36 @@ def apply_alpha_gradient_to_image(image_path):
     third_gradient_height = third_end_height - third_start_height
     fourth_gradient_height = fourth_end_height - fourth_start_height
 
-    # 첫 번째 그라데이션 알파 값
-    alpha1 = np.linspace(1, 0.3, first_gradient_height).reshape(-1, 1)
-    alpha1 = np.repeat(alpha1, w, axis=1)
-    alpha1 = np.stack([alpha1] * 3, axis=2)  # RGB 채널 적용
+    # 알파 값 생성
+    def create_alpha_array(start, end, height, width):
+        alpha = np.linspace(start, end, height).reshape(-1, 1)
+        alpha = np.repeat(alpha, width, axis=1)
+        return alpha
 
-    # 두 번째 그라데이션 알파 값
-    alpha2 = np.linspace(0.3, 0.2, second_gradient_height).reshape(-1, 1)
-    alpha2 = np.repeat(alpha2, w, axis=1)
-    alpha2 = np.stack([alpha2] * 3, axis=2)  # RGB 채널 적용
+    alpha1 = create_alpha_array(1, 0.3, first_gradient_height, w)
+    alpha2 = create_alpha_array(0.3, 0.2, second_gradient_height, w)
+    alpha3 = create_alpha_array(0.2, 0.1, third_gradient_height, w)
+    alpha4 = create_alpha_array(0.1, 0, fourth_gradient_height, w)
 
-    # 세 번째 그라데이션 알파 값
-    alpha3 = np.linspace(0.2, 0.1, third_gradient_height).reshape(-1, 1)
-    alpha3 = np.repeat(alpha3, w, axis=1)
-    alpha3 = np.stack([alpha3] * 3, axis=2)  # RGB 채널 적용
-
-    # 네 번째 구간 알파 값
-    alpha4 = np.linspace(0.1, 0, third_gradient_height).reshape(-1, 1)
-    alpha4 = np.repeat(alpha4, w, axis=1)
-    alpha4 = np.stack([alpha4] * 3, axis=2)  # RGB 채널 적용
-
-    # 검정색 배경 생성
-    black_background = np.zeros((h, w, 3), dtype=np.uint8)
+    black_background = Image.new("RGB", (w, h), (0, 0, 0))
 
     result_image = image.copy()
 
-    # 원본 이미지의 첫 번째 하단 부분을 알파 블렌딩
-    result_image[first_start_height:first_end_height] = (
-            image[first_start_height:first_end_height] * alpha1 + black_background[
-                                                                  first_start_height:first_end_height] * (1 - alpha1)
-    ).astype(np.uint8)
+    def blend_image_section(image, alpha, start_height, end_height):
+        section = image.crop((0, start_height, w, end_height))
+        black_section = black_background.crop((0, start_height, w, end_height))
+        alpha_img = Image.fromarray((alpha * 255).astype(np.uint8), mode='L')
+        blended_section = Image.composite(section, black_section, alpha_img)
+        image.paste(blended_section, (0, start_height))
 
-    # 원본 이미지의 두 번째 하단 부분을 알파 블렌딩
-    result_image[second_start_height:second_end_height] = (
-            image[second_start_height:second_end_height] * alpha2 + black_background[
-                                                                    second_start_height:second_end_height] * (
-                    1 - alpha2)
-    ).astype(np.uint8)
+    blend_image_section(result_image, alpha1, first_start_height, first_end_height)
+    blend_image_section(result_image, alpha2, second_start_height, second_end_height)
+    blend_image_section(result_image, alpha3, third_start_height, third_end_height)
+    blend_image_section(result_image, alpha4, fourth_start_height, fourth_end_height)
 
-    # 원본 이미지의 세 번째 하단 부분을 알파 블렌딩
-    result_image[third_start_height:third_end_height] = (
-            image[third_start_height:third_end_height] * alpha3 + black_background[
-                                                                  third_start_height:third_end_height] * (1 - alpha3)
-    ).astype(np.uint8)
-
-    # 원본 이미지의 네 번째 하단 부분을 알파 블렌딩 (0으로 유지)
-    result_image[fourth_start_height:fourth_end_height] = (
-            image[fourth_start_height:fourth_end_height] * alpha4 + black_background[
-                                                                    fourth_start_height:fourth_end_height] * (
-                    1 - alpha4)
-    ).astype(np.uint8)
-    # 결과 저장
-    cv2.imwrite(prefix_after_processing_path + image_name + ".png", result_image)
+    output_path = os.path.join(prefix_after_processing_path, image_name + ".png")
+    result_image.save(output_path)
     return result_image
-
 
 def divide_text_for_one_page(text, font, line_spacing):
     """
@@ -273,11 +335,17 @@ def add_text_to_image(image, text, position, font, box_size, text_color, line_sp
         y += draw.textbbox((0, 0), line, font=font)[3] + line_spacing
 
 
-def resize_alpha_adjust(image_path):
-    p1_image = resize_image(image_path)
+def resize_alpha_adjust_type1(image_path):
+    processing1_image = resize_image_type1(image_path)
     image_name = os.path.splitext(os.path.basename(image_path))[0]
-    p1_path = prefix_after_processing_path + image_name + ".png"
-    apply_alpha_gradient_to_image(p1_path)
+    processing1_path = prefix_after_processing_path + image_name + ".png"
+    apply_alpha_gradient_to_image_type1(processing1_path)
+
+# def resize_alpha_adjust_type2(image_path):
+#     p1_image = resize_image_type1(image_path)
+#     image_name = os.path.splitext(os.path.basename(image_path))[0]
+#     p1_path = prefix_after_processing_path + image_name + ".png"
+#     apply_alpha_gradient_to_image(p1_path)
 
 
 def create_main_content_type1(image_path, text, font, line_spacing):
@@ -319,6 +387,46 @@ def create_main_content_type1(image_path, text, font, line_spacing):
     output_path = prefix_after_processing_path + image_name + ".png"
     result_image.save(output_path)
 
+def create_main_content_type2(image_path, text, font, line_spacing):
+    """
+        이미지에 텍스트 박스를 추가하고 그 안에 텍스트를 작성합니다.
+        """
+    image_name = os.path.splitext(os.path.basename(image_path))[0]
+    position = (120, 800)  # 텍스트를 추가할 위치 (x, y)
+    box_size = (900, 370)  # 텍스트 박스 크기 (width, height)
+    text_color = (255, 255, 255)  # 흰색
+    box_color = (255, 255, 255)
+
+    # 이미지 로드
+    image = Image.open(image_path).convert('RGBA')
+    draw = ImageDraw.Draw(image)
+
+    # 텍스트 줄바꿈 처리
+    lines = []
+    words = text.split(' ')
+    max_width, max_height = box_size
+    x, y = position
+
+    # draw.rectangle([x, y, x + max_width, y + max_height], fill=box_color)
+    while words:
+        line = ''
+        while words and draw.textbbox((0, 0), line + words[0], font=font)[2] <= max_width:
+            if (words[0].endswith(".")):
+                line = line + (words.pop(0) + ' ')
+                break
+            line = line + (words.pop(0) + ' ')
+        lines.append(line)
+
+    for line in lines:
+        draw.text((x, y), line, font=font, fill=text_color)
+        y += draw.textbbox((0, 0), line, font=font)[3] + line_spacing
+
+    result_image = image.convert('RGB')  # RGBA를 RGB로 변환
+    # 결과 저장
+    output_path = prefix_after_processing_path + image_name + ".png"
+    result_image.save(output_path)
+
+
 def create_title_image(image_path, title, sub_title, article_type):
     title_box_size = (900, 174)
     sub_title_box_size = (850, 116)
@@ -332,7 +440,7 @@ def create_title_image(image_path, title, sub_title, article_type):
     fix_size_value = 3
     while title_font_size > min_title_font_size and sub_title_font_size > min_sub_title_font_size:
         try:
-            add_basic_icon_to_image(image_path, article_type)
+            add_title_icon_to_image(image_path, article_type)
             image = Image.open(image_path).convert('RGBA')
             title_font = ImageFont.truetype(godic_font, title_font_size)
             sub_title_font = ImageFont.truetype(godic_font, sub_title_font_size)
@@ -381,17 +489,17 @@ def create_content_image(text, image_path_list):
     image_count = len(image_path_list)
     select_image_index_list = select_image_index(need_page_count, image_count)
     for index, text_for_one_page in enumerate(divided_text):
-        # 이미지 여러개 나눌 로직 짜기..
         image_index = select_image_index_list[index]
         create_main_content_type1(image_path_list[image_index], text_for_one_page, font, line_spacing)
 
 
-# # 이미지 경로
-# image_path = prefix_after_processing_path + 'Carlos_Sainz_and_Charles_Leclerc_of_Ferrari_fter_the_Formula_1_Spanish_Grand_Prix_at_Circuit_de.png'
-# before_image_path1 = "../download_image/BARCELONA,_SPAIN_-_JUNE_23__Second_placed_Lando_Norris_of_Great_Britain_and_McLaren_smiles_with_his.jpg"
-#
-#
-# # 이미지 로드
+# 이미지 경로
+image_path = prefix_after_processing_path + 'Carlos_Sainz_and_Charles_Leclerc_of_Ferrari_fter_the_Formula_1_Spanish_Grand_Prix_at_Circuit_de.png'
+before_image_path1 = "../download_image/Carlos_Sainz_and_Charles_Leclerc_of_Ferrari_fter_the_Formula_1_Spanish_Grand_Prix_at_Circuit_de.jpg"
+
+resize_alpha_adjust_type1(before_image_path1)
+
+# 이미지 로드
 # image = cv2.imread(image_path, cv2.IMREAD_COLOR)
 # # 텍스트 추가
 # text = "베르스타펜은 '차를 커브에 올리기 힘들어 시간 손실이 크다'고 말했는데요, 중고속 구간에서는 편안함을 느꼈지만 저속 구간에서 시간 손실이 컸다고 덧붙였습니다. 일요일 레이스에서 78랩의 경주를 치르며 drama를 대비할 계획이라고 밝혔습니다. 일요일 레이스에서 78랩의 경주를 치르며 drama를 대비할 계획이라고 밝혔습니다. 일요일 레이스에서 78랩의 경주를 치르며 drama를 대비할 계획이라고 밝혔습니다. 일요일 레이스에서 78랩의 경주를 치르며 drama를 대비할 계획이라고 밝혔습니다. 일요일 레이스에서 78랩의 경주를 치르며 drama를 대비할 계획이라고 밝혔습니다. 일요일 레이스에서 78랩의 경주를 치르며 drama를 대비할 계획이라고 밝혔습니다. 일요일 레이스에서 78랩의 경주를 치르며 drama를 대비할 계획이라고 밝혔습니다. 일요일 레이스에서 78랩의 경주를 치르며 drama를 대비할 계획이라고 밝혔습니다."
