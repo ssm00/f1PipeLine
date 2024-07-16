9f22881c27c76d4ac2e446e44e991475162c1290
ssm00 <titan0724@mju.ac.kr>
2024-07-15 20:20:13 -0600

    Update ImageArticleEditor.py

--- ImageModifier/ImageArticleEditor.py
+++ ImageModifier/ImageArticleEditor.py
@@ -138,43 +138,83 @@ def add_title_icon_to_image(image_path, article_type):
     output_path = os.path.join(prefix_after_processing_path, image_name + ".png")
     image_with_line.save(output_path, "PNG")
 
-def resize_image_type1(image_path):
-    """
-    1080, 1350 resize
-    """
+# def resize_image_type1(image_path):
+#     """
+#     1080, 1350 resize
+#     """
+#     image_name = os.path.splitext(os.path.basename(image_path))[0]
+#     image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+#     h, w = image.shape[:2]
+#     target_size = (1080, 1350)
+#     target_height = target_size[1]
+#     target_width = target_size[0]
+#     proportion_h = target_size[1] / h
+#     proportion_w = target_size[0] / w
+#     # 먼저 가로 세로 중 작은 부분을 target_size에 맞게 사진을 늘려야함
+#     # 가로나 세로중 하나를 항상 기준으로 하면 크기가 큰 사진이 줄어드는 경우 가로 세로중 더 짧은 쪽은 검정 target보다 작아 공백이 되는 경우가 발생할 수 있음. 케이스 분리필요함
+#     # 가로가 긴 사진인 경우
+#     if w > h:
+#         # 세로를 정해진 비율 만큼 늘리거나 줄이기 가로는 어차피 target_w 넘어감
+#         new_height = target_height + 100
+#         new_width = int(proportion_h * w)
+#         resized_image = cv2.resize(image, (new_width, new_height))
+#         # 사진 스타트 포인트 구하기 (new_width / 2) - (target_with / 2) = (new_width - target_width) / 2
+#         start_x = (new_width - target_width) // 2
+#         # 세로 전체, 가로는 start_x 부터 start_x + target_width 까지
+#         resized_cropped_image = resized_image[100:new_height, start_x: start_x + target_width]
+#     # 세로가 긴 사진인 경우
+#     else:
+#         # 가로를 정해진 비율 만큼 늘리거나 줄이기 세로는 어차피 target_h 넘어감
+#         new_height = int(proportion_w * h)
+#         new_width = target_width
+#         resized_image = cv2.resize(image, (new_width, new_height))
+#         # 사진 스타트 포인트 구하기 (new_width / 2) - (target_with / 2) = (new_width - target_width) / 2
+#         start_y = (new_height - target_height) // 2
+#         # 세로 전체, 가로는 start_x 부터 start_x + target_width 까지
+#         # 사진은 0,0 이 우상단임
+#         resized_cropped_image = resized_image[start_y + 100:start_y + target_height + 100, :]
+#     cv2.imwrite(prefix_after_processing_path + image_name + ".png", resized_cropped_image)
+#     return resized_cropped_image
+
+def resize_image_type1(image_path, image_id):
     image_name = os.path.splitext(os.path.basename(image_path))[0]
-    image = cv2.imread(image_path, cv2.IMREAD_COLOR)
-    h, w = image.shape[:2]
+    image = Image.open(image_path)
+    w, h = image.size
     target_size = (1080, 1350)
     target_height = target_size[1]
     target_width = target_size[0]
     proportion_h = target_size[1] / h
     proportion_w = target_size[0] / w
-    # 먼저 가로 세로 중 작은 부분을 target_size에 맞게 사진을 늘려야함
-    # 가로나 세로중 하나를 항상 기준으로 하면 크기가 큰 사진이 줄어드는 경우 가로 세로중 더 짧은 쪽은 검정 target보다 작아 공백이 되는 경우가 발생할 수 있음. 케이스 분리필요함
+
     # 가로가 긴 사진인 경우
     if w > h:
-        # 세로를 정해진 비율 만큼 늘리거나 줄이기 가로는 어차피 target_w 넘어감
         new_height = target_height + 100
         new_width = int(proportion_h * w)
-        resized_image = cv2.resize(image, (new_width, new_height))
-        # 사진 스타트 포인트 구하기 (new_width / 2) - (target_with / 2) = (new_width - target_width) / 2
+        resized_image = image.resize((new_width, new_height), Image.ANTIALIAS)
         start_x = (new_width - target_width) // 2
-        # 세로 전체, 가로는 start_x 부터 start_x + target_width 까지
-        resized_cropped_image = resized_image[100:new_height, start_x: start_x + target_width]
+        resized_cropped_image = resized_image.crop((start_x, 100, start_x + target_width, new_height))
     # 세로가 긴 사진인 경우
     else:
-        # 가로를 정해진 비율 만큼 늘리거나 줄이기 세로는 어차피 target_h 넘어감
         new_height = int(proportion_w * h)
         new_width = target_width
-        resized_image = cv2.resize(image, (new_width, new_height))
-        # 사진 스타트 포인트 구하기 (new_width / 2) - (target_with / 2) = (new_width - target_width) / 2
+        resized_image = image.resize((new_width, new_height), Image.ANTIALIAS)
         start_y = (new_height - target_height) // 2
-        # 세로 전체, 가로는 start_x 부터 start_x + target_width 까지
-        # 사진은 0,0 이 우상단임
-        resized_cropped_image = resized_image[start_y + 100:start_y + target_height + 100, :]
-    cv2.imwrite(prefix_after_processing_path + image_name + ".png", resized_cropped_image)
+        resized_cropped_image = resized_image.crop((0, start_y + 100, new_width, start_y + target_height + 100))
+
+    save_path = os.path.join(prefix_after_processing_path, str(image_id))
+    os.makedirs(save_path, exist_ok=True)
+    resized_cropped_image.save(os.path.join(save_path, image_name + ".png"))
     return resized_cropped_image
+
+
+def resize_image_type2(image_path):
+    """
+        가로로 긴 사진 생성 사진이 새로 형식 사진이라면 이미지 비율이 너무 안맞아서 불가능 그냥 return 하기\
+        2160, 1350 resize
+    """
+
+
+
 
 # def apply_alpha_gradient_to_image(image_path):
 #     """
