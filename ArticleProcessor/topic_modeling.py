b465c6f8ad8251252f247b937d3a66aa36562c2f
ssm00 <titan0724@mju.ac.kr>
2024-09-13 13:58:49 +0900

    Update topic_modeling.py

--- ArticleProcessor/topic_modeling.py
+++ ArticleProcessor/topic_modeling.py
@@ -1,16 +1,16 @@
-import pymysql
-import nltk
 from Db import f1Db
+import tqdm
 import json
-from nltk.tokenize import sent_tokenize
-from nltk.tokenize import word_tokenize
-from gensim.parsing.preprocessing import remove_stopwords
-from nltk.stem import WordNetLemmatizer
 import string
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
-from nltk.tokenize import TreebankWordTokenizer
 from nltk.stem import PorterStemmer
-from collections import Counter
+import gensim
+from gensim import corpora
+from gensim.models.coherencemodel import CoherenceModel
+import matplotlib.pyplot as plt
 
 with open('../Db/db_info.json', 'r') as file:
     db_info_json = json.load(file)
@@ -25,13 +25,12 @@ def download_nltk_package():
     nltk.download('averaged_perceptron_tagger_eng')
     nltk.download('punkt_tab')
 
-def get_content1(db_info):
+def update_bow(db_info):
     database = f1Db.Database(db_info)
     original_content_query = "select original_content from article where sequence = 2"
     database.execute(query=original_content_query)
     res = database.cursor.fetchall()
     content = res[0].get('original_content')
-    #tokens = TreebankWordTokenizer().tokenize(content)
 
     tokens = word_tokenize(content)
 
@@ -54,19 +53,49 @@ def get_content1(db_info):
     stemmed = [stemmer.stem(token) for token in tokens_without_punctuation]
 
     tag_list = nltk.pos_tag(without_lemma)
-    #nn, nnp, nns, nnps, vb, vbd,
 
+    #nn, nnp, nns, nnps, vb, vbd,
     need_tags = ["NN","NNP","NNS","NNPS","VB","VBD"]
-    res = []
+    res_list = []
     for tag in tag_list:
         if tag[1] in need_tags:
-            res.append(tag)
-
-    bow = Counter([f"{word}/{pos_tag}" for word, pos_tag in res])
-    bow_json = json.dumps(bow)
+            res_list.append(tag[0])
+    res = ",".join(res_list)
     query = "update article set bow = %s where sequence = %s"
-    values = bow_json, 2
+    values = res, 2
     database.cursor.execute(query, values)
-    #database.commit()
+    database.commit()
 
-get_content1(mysql_db)
+def get_all_bow(db_info):
+    database = f1Db.Database(db_info)
+    select_all_bow = "select bow from article where sequence = 2"
+    bow_list = database.fetch_all(select_all_bow)
+    all_word_list = []
+    for bow in bow_list:
+        bow = bow.get("bow")
+        all_word_list.append(bow.split(","))
+
+    #corpora의 매개변수로는 문서의 각 단어를 담고 있는리스트를 담고있는 전체 리스트. 즉 문서마다 리스트로 분리 해야함, 하나의 리스트에 모든 단어 다넣으면 하나의 문서로 인식함
+    dictionary = corpora.Dictionary(all_word_list)
+    corpus = [dictionary.doc2bow(doc) for doc in tqdm.tqdm(all_word_list)]
+
+    #토픽모델링
+    #NUM_TOPICS = 20  # 20개의 토픽, k=20
+    #ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=NUM_TOPICS, id2word=dictionary, passes=15)
+    #topics = ldamodel.print_topics(num_words=4)
+    coherence_values = []
+    for i in range(2, 15):
+        ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=i, id2word=dictionary, passes=15)
+        coherence_model_lda = CoherenceModel(model=ldamodel, texts=all_word_list, dictionary=dictionary, topn=10)
+        coherence_lda = coherence_model_lda.get_coherence()
+        coherence_values.append(coherence_lda)
+    x = range(2, 15)
+    plt.plot(x, coherence_values)
+    plt.xlabel("number of topics")
+    plt.ylabel("coherence score")
+    plt.show()
+
+
+#update_bow(mysql_db)
+get_all_bow(mysql_db)
+
