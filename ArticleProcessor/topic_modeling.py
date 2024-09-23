e1c379ffdbf01c1f317a7d0f39a3b8ec6a719f31
ssm00 <titan0724@mju.ac.kr>
2024-09-23 14:16:51 +0900

    Update topic_modeling.py

--- ArticleProcessor/topic_modeling.py
+++ ArticleProcessor/topic_modeling.py
@@ -21,7 +21,7 @@ from datetime import datetime
 
 
 from multiprocessing import Process, freeze_support
-
+from collections import defaultdict
 
 with open('../Db/db_info.json', 'r') as file:
     db_info_json = json.load(file)
@@ -80,12 +80,21 @@ def update_bow(db_info):
 def get_all_bow(db_info):
     start = time.time()
     database = f1Db.Database(db_info)
-    select_all_bow = "select bow from article where Date(created_at) = %s"
-    today = datetime.now().date()
-    bow_list = database.fetch_all(select_all_bow, today)
+    # select_all_bow = "select sequence, bow from article where Date(created_at) = %s"
+    # today = datetime.now().date()
+    # bow_list = database.fetch_all(select_all_bow, today)
+    #
+    #dev
+    select_all_bow_dev = "select sequence, bow from article where Date(created_at) = %s"
+    dev_date = "2024-09-16"
+    data_list = database.fetch_all(select_all_bow_dev, dev_date)
+
     all_word_list = []
-    for bow in bow_list:
+    id_list = []
+    for bow in data_list:
+        id = bow.get("sequence")
         bow = bow.get("bow")
+        id_list.append(id)
         all_word_list.append(bow.split(","))
     print(len(all_word_list))
     #corpora의 매개변수로는 문서의 각 단어를 담고 있는리스트를 담고있는 전체 리스트. 즉 문서마다 리스트로 분리 해야함, 하나의 리스트에 모든 단어 다넣으면 하나의 문서로 인식함
@@ -93,36 +102,53 @@ def get_all_bow(db_info):
     corpus = [dictionary.doc2bow(doc) for doc in tqdm(all_word_list)]
 
     #토픽모델링
-    #NUM_TOPICS = 20  # 20개의 토픽, k=20
-    #ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=NUM_TOPICS, id2word=dictionary, passes=15)
-    #topics = ldamodel.print_topics(num_words=4)
+    dev_modeling_range = range(4,5)
+
+    modeling_range = range(2, 20, 2)
     coherence_values = []
     model_list = []
-    for i in tqdm(range(2, 20, 2)):
+    for i in tqdm(dev_modeling_range):
         # passes : 최대 반복 횟수
         ldamodel = LdaModel(corpus, num_topics=i, id2word=dictionary, passes=5)
         model_list.append(ldamodel)
         coherence_model_lda = CoherenceModel(model=ldamodel, texts=all_word_list, dictionary=dictionary, coherence='c_v')
         coherence_lda = coherence_model_lda.get_coherence()
         coherence_values.append(coherence_lda)
-    x = range(2, 20, 2)
     best_model_index = coherence_values.index(max(coherence_values))
-    print(best_model_index)
     best_model = model_list[best_model_index]
+    # doc_topics 토픽의 분포 [(0, 0.4), (1, 0.3), (2, 0.2), (3, 0.1)] 첫번째 토픽에 40프로 분포
+    doc_topics = best_model[corpus]
+    find_best_source(doc_topics, id_list)
+    #
     end = time.time()
-    plt.plot(x, coherence_values)
-    plt.xlabel("number of topics")
-    plt.ylabel("coherence score")
-    plt.show()
+    print(best_model[corpus])
+    print(f"{end - start:.5f} sec")
+
+def visualization_topic(best_model, corpus, dictionary):
     vis = pyLDAvis.gensim.prepare(best_model, corpus, dictionary, sort_topics=False)
     pyLDAvis.save_html(vis, './lda.html')
-    print(f"{end - start:.5f} sec")
-    pickle.dump(corpus, open('../data/lda/lda_corpus.pkl', 'wb'))
-    dictionary.save('./data/lda/lda_dictionary.gensim')
-    best_model.save('./data/lda/lda_model.gensim')
+
+def find_best_source(doc_topics, id_list):
+    topic_to_pks_with_probs = defaultdict(list)
+    for doc_index, topics in enumerate(doc_topics):
+        for topic_id, prob in topics:
+            topic_to_pks_with_probs[topic_id].append((id_list[doc_index], prob))
+    # 각 토픽별로 확률이 높은 순으로 상위 5개의 문서 선택
+    top_n = 5  # 상위 5개의 문서를 선택
+    topic_top_articles = {}
+    for topic_id, pk_prob_list in topic_to_pks_with_probs.items():
+        # 확률 기준으로 정렬 (내림차순)
+        sorted_pks_by_prob = sorted(pk_prob_list, key=lambda x: x[1], reverse=True)
+
+        # 상위 5개의 문서 선택
+        top_articles = sorted_pks_by_prob[:top_n]
+
+        # 토픽별 상위 5개 문서의 PK 저장
+        topic_top_articles[topic_id] = [pk for pk, prob in top_articles]
+
 
 
 if __name__ == '__main__':
   freeze_support()
-  update_bow(mysql_db)
+  #update_bow(mysql_db)
   Process(target=get_all_bow(mysql_db)).start()
