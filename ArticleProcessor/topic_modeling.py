db31eb554e7550fe452c060a3cf1c933ba34e7d0
ssm00 <titan0724@mju.ac.kr>
2024-09-25 14:59:03 +0900

    Update topic_modeling.py

--- ArticleProcessor/topic_modeling.py
+++ ArticleProcessor/topic_modeling.py
@@ -1,7 +1,8 @@
-import pickle
+import spacy
 
 from Db import f1Db
 from tqdm import tqdm
+from datetime import datetime
 import json
 import string
 import nltk
@@ -11,13 +12,16 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
+from nltk import ne_chunk
+from nltk.tree import Tree
+
 from gensim import corpora
 from gensim.models.coherencemodel import CoherenceModel
 from gensim.models.ldamodel import LdaModel
+
 import matplotlib.pyplot as plt
 import pyLDAvis
 import pyLDAvis.gensim
-from datetime import datetime
 
 
 from multiprocessing import Process, freeze_support
@@ -35,10 +39,12 @@ def download_nltk_package():
     nltk.download('tagsets') # 품사태깅
     nltk.download('averaged_perceptron_tagger_eng')
     nltk.download('punkt_tab')
+    nltk.download('maxent_ne_chunker_tab')
+    nltk.download('words')
 
 
 def exe_bow(content):
-    content = content.get('original_content')
+    # content = content.get('original_content')
     tokens = word_tokenize(content)
     # 구두점 + 추가적인 제거하고 싶은 특수문자들
     additional_punctuations = set(string.punctuation) | {'“', '”', '’', "'", '"', '‘', "-",
@@ -55,14 +61,53 @@ def exe_bow(content):
     stemmer = PorterStemmer()
     stemmed = [stemmer.stem(token) for token in tokens_without_punctuation]
     tag_list = nltk.pos_tag(without_lemma)
+
+    #사람이름 추출
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(content)
+    person_names = [ent.text for ent in doc.ents if ent.label_ == "PERSON"]
+
+    with open("./f1_name_list.json", "r") as file:
+        f1_name_list_json = json.load(file)
+        f1_name_list = f1_name_list_json.get("people")
+
+    driver_list = []
+    engineer_list = []
+    director_list = []
+    ceo_list = []
+    f1_name_category = {
+        "driver": driver_list,
+        "engineer": engineer_list,
+        "director": director_list,
+        "ceo": ceo_list
+    }
+    for people_type, name_list in f1_name_list.items():
+        if people_type in f1_name_category:
+            f1_name_category[people_type].extend(name for full_name in name_list for name in full_name.split(" "))
     # nn, nnp, nns, nnps, vb, vbd,
     need_tags = ["NN", "NNP", "NNS", "NNPS", "VB", "VBD"]
+
     res_list = []
     for tag in tag_list:
-        if tag[1] in need_tags:
+        if tag[1] in need_tags and tag[0] in driver_list:
+            res_list.extend([tag[0]] * 3)
+        elif tag[1] in need_tags and tag[0] in director_list:
+            res_list.extend([tag[0]] * 3)
+        elif tag[1] in need_tags and tag[0] in engineer_list:
+            res_list.extend([tag[0]] * 2)
+        elif tag[1] in need_tags and tag[0] in ceo_list:
+            res_list.extend([tag[0]] * 2)
+        elif tag[1] in need_tags:
             res_list.append(tag[0])
     res = ",".join(res_list)
     return res
+
+def spyc(text):
+    nlp = spacy.load('en_core_web_sm')
+    doc = nlp(text)
+    person_names = [ent.text for ent in doc.ents if ent.label_ == 'PERSON']
+    print(person_names)
+    return person_names
 
 def update_bow(db_info):
     database = f1Db.Database(db_info)
@@ -76,38 +121,30 @@ def update_bow(db_info):
         database.cursor.execute(query, values)
     database.commit()
 
-
 def get_all_bow(db_info):
     start = time.time()
     database = f1Db.Database(db_info)
-    # select_all_bow = "select sequence, bow from article where Date(created_at) = %s"
-    # today = datetime.now().date()
-    # bow_list = database.fetch_all(select_all_bow, today)
-    #
-    #dev
-    select_all_bow_dev = "select sequence, bow from article where Date(created_at) = %s"
-    dev_date = "2024-09-16"
-    data_list = database.fetch_all(select_all_bow_dev, dev_date)
+    select_all_bow = "select sequence, bow from article where Date(created_at) = %s"
+    today = datetime.now().date()
+    bow_list = database.fetch_all(select_all_bow, today)
 
     all_word_list = []
     id_list = []
-    for bow in data_list:
+    for bow in bow_list:
         id = bow.get("sequence")
         bow = bow.get("bow")
         id_list.append(id)
         all_word_list.append(bow.split(","))
-    print(len(all_word_list))
+
     #corpora의 매개변수로는 문서의 각 단어를 담고 있는리스트를 담고있는 전체 리스트. 즉 문서마다 리스트로 분리 해야함, 하나의 리스트에 모든 단어 다넣으면 하나의 문서로 인식함
     dictionary = corpora.Dictionary(all_word_list)
     corpus = [dictionary.doc2bow(doc) for doc in tqdm(all_word_list)]
 
     #토픽모델링
-    dev_modeling_range = range(4,5)
-
     modeling_range = range(2, 20, 2)
     coherence_values = []
     model_list = []
-    for i in tqdm(dev_modeling_range):
+    for i in tqdm(modeling_range):
         # passes : 최대 반복 횟수
         ldamodel = LdaModel(corpus, num_topics=i, id2word=dictionary, passes=5)
         model_list.append(ldamodel)
@@ -119,7 +156,8 @@ def get_all_bow(db_info):
     # doc_topics 토픽의 분포 [(0, 0.4), (1, 0.3), (2, 0.2), (3, 0.1)] 첫번째 토픽에 40프로 분포
     doc_topics = best_model[corpus]
     find_best_source(doc_topics, id_list)
-    #
+
+    #time checker
     end = time.time()
     print(best_model[corpus])
     print(f"{end - start:.5f} sec")
@@ -145,7 +183,8 @@ def find_best_source(doc_topics, id_list):
 
         # 토픽별 상위 5개 문서의 PK 저장
         topic_top_articles[topic_id] = [pk for pk, prob in top_articles]
-
+    for topic_id, top_pks in topic_top_articles.items():
+        print(f"토픽 {topic_id}를 가장 잘 나타내는 문서들의 PK: {top_pks}")
 
 
 if __name__ == '__main__':
