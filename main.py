74d91a4ea09e8a96141c129e4a53b0f93e1d76ef
ssm00 <titan0724@mju.ac.kr>
2024-10-10 14:18:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import json
 
+from ImageModifier.ImageArticleEditor import ImageGenerator
 from ArticleCrawler.F1PageCrawler import F1PageCrawler
 from ArticleProcessor.Topic_modeling import TopicModeling
 from multiprocessing import Process, freeze_support
-from datetime import datetime
-from datetime import timedelta
 from ArticleProcessor.ArticleTranslator import ArticleTranslator
 from Db import f1Db
-from ImageModifier.ImageArticleEditor import ImageGenerator
+
+from tqdm import tqdm
 
 with open('./MyMetaData/db_info.json', 'r') as file:
     db_info_json = json.load(file)
@@ -34,8 +34,7 @@ class F1Main:
         self.database = database
 
     def test_crawling(self):
-
-        for i in range(1,3):
+        for i in range(1,5):
             self.f1_crawler.run(i)
 
     def test_topic_modeling(self):
@@ -45,68 +44,85 @@ class F1Main:
         dynamic_topic_range = (dynamic_topic_number_range.get("start"), dynamic_topic_number_range.get("end"), dynamic_topic_number_range.get("step"))
         self.topic_modeling.run(date_range, dynamic_topic_range)
 
+    def make_img(self, translate_content, article_sequence, article_id, article_type):
+        paragraph_list = translate_content.get("paragraph")
+        recommend_title = translate_content.get("recommendTitle")
+        click_bait_title = translate_content.get("clickBaitTitle")
+        final_sentence = translate_content.get("finalSentence")
+        text = ""
+        for paragraph, content in paragraph_list.items():
+            text = text + content + " "
+        text += final_sentence
+        image_list = database.get_images_by_article_id(article_id)
+        image_path_list = []
+        for image in image_list:
+            image_path_list.append(image_generator_info.get("image_source_path") + image.get("image_name") + ".png")
+        ######### 이미지 없는 경우 필터링 IndexError
+        self.image_generator.create_title_image(image_path_list[0], recommend_title, click_bait_title, article_type, article_sequence)
+        self.image_generator.create_main_content(text, image_path_list, article_type, article_sequence)
 
     def v1_one_article(self):
-        database = f1Db.Database(mysql_db)
-
         #crawlinwg
-        #self.test_crawling()
+        self.test_crawling()
 
         #get one
-        result = database.get_one_article(crawler_properties_json.get("total_crawling_date_from_today"))
+        result = self.database.get_one_article_by_date_range(crawler_properties_json.get("total_crawling_date_from_today"))
         #translator
         prompt_v1 = prompt_json.get("prompt_v1")
         key = key_json.get("open_ai_api_key")
         article_translator = ArticleTranslator(prompt_v1, key)
 
         sequence = result.get("sequence")
-        original_title = result.get("original_title")
         original_content = result.get("original_content")
         article_type = result.get("article_type")
         article_id = result.get("article_id")
-        translated_content_json = article_translator.translate_v1(original_content)
-        translated_content = json.loads(translated_content_json)
-        recommend_title = translated_content.get("recommendTitle")
-        print(translated_content)
-        database.update_translate_content(sequence, translated_content_json)
+        translate_content_json = article_translator.translate_v1(original_content)
+        translate_content = json.loads(translate_content_json)
+        self.make_img(translate_content, sequence, article_id, article_type)
+        
+        #db translate_content 업데이트
+        database.update_translate_content(sequence, translate_content_json)
 
-    def make_img(self):
-        database = f1Db.Database(mysql_db)
-        article = database.get_translate_content(478)
-        translate_content = json.loads(article.get("translate_content"))
-        paragraph_list = translate_content.get("paragraph")
-        text = ""
-        for paragraph, content in paragraph_list.items():
-            text += content + "\n"
-        image_list = database.get_images_by_article_id("4xrZ1DlFphcguA6lmabrIE")
-        image_path_list = []
-        for image in image_list:
-            image_path_list.append(image_generator_info.get("image_source_path") + image.get("image_name") + ".png")
-        self.image_generator.create_main_content(text, image_path_list, "Information", 478)
-    # def v2():
-    #     test_crawling()
-    #     date_range = crawler_properties_json.get("total_crawling_date_from_today")
-    #     now = datetime.now()
-    #     start_date = now - timedelta(days=date_range - 1)
-    #     end_date = now + timedelta(days=1)
-    #
-    #     get_today_article = "select * from article where published_at between Date(%s) and Date(%s)"
-    #     values = (start_date, end_date)
-    #
-    #     # translator
-    #     prompt_v1 = prompt_json['prompt_v1']
-    #     key = key_json['api_key']
-    #     article_translator = ArticleTranslator(prompt_v1, key)
-    #
-    #     article_translator.translate_v1(content)
-    #
-    #     get_article_id =
-    #     article_images =
+    def v1_all_article(self):
+        # crawling
+        #self.test_crawling()
+
+        # get all
+        article_list = self.database.get_all_article_by_date_range(crawler_properties_json.get("total_crawling_date_from_today"))
+        # translator
+        prompt_v1 = prompt_json.get("prompt_v1")
+        key = key_json.get("open_ai_api_key")
+        article_translator = ArticleTranslator(prompt_v1, key)
+        for article in tqdm(article_list):
+            sequence = article.get("sequence")
+            original_content = article.get("original_content")
+            article_type = article.get("article_type")
+            article_id = article.get("article_id")
+
+            translate_content_json = article_translator.translate_v1(original_content)
+            translate_content = json.loads(translate_content_json)
+            database.update_translate_content(sequence, translate_content_json)
+            self.make_img(translate_content, sequence, article_id, article_type)
+            # db translate_content 업데이트
+
+    def v1_one_article_sequence(self, sequence):
+
+        # get one
+        result = self.database.get_one_by_sequence(sequence)
+        # translator
+
+        sequence = result.get("sequence")
+        original_content = result.get("original_content")
+        article_type = result.get("article_type")
+        article_id = result.get("article_id")
+        translate_content_json = result.get("translate_content")
+        translate_content = json.loads(translate_content_json)
+        self.make_img(translate_content, sequence, article_id, article_type)
 
 if __name__ == '__main__':
     freeze_support()
     database = f1Db.Database(mysql_db)
     main = F1Main(database)
-    #main.v1_one_article()
-    main.make_img()
+    main.v1_all_article()
+    #main.v1_one_article_sequence(512)
 
