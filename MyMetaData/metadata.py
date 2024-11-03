f3739e3cb1cd8f4314de7d80733889754c6e7dc9
ssm00 <titan0724@mju.ac.kr>
2024-11-03 14:10:40 +0900

    feat : 메타데이터 로드 클래스 추가

---
+++ MyMetaData/metadata.py
@@ -0,0 +1,31 @@
+import json
+from pathlib import Path
+
+class MetaData:
+
+    def __init__(self, config_dir='./MyMetaData'):
+        self.image_generator_info = None
+        self.prompt = None
+        self.key = None
+        self.topic_modeling_properties = None
+        self.crawler_properties = None
+        self.db_info = None
+        self.scheduler_info = None
+        self.config_dir = Path(config_dir)
+        self.load_all_json()
+
+    def load_json(self, file_name):
+        file_path = self.config_dir / file_name
+        with open(file_path, 'r', encoding='utf-8') as f:
+            return json.load(f)
+
+
+    def load_all_json(self):
+        self.db_info = self.load_json('db_info.json')
+        self.crawler_properties = self.load_json('crawler_properties.json')
+        self.topic_modeling_properties = self.load_json('topic_modeling_properties.json')
+        self.key = self.load_json('key.json')
+        self.prompt = self.load_json('prompt.json')
+        self.image_generator_info = self.load_json('image_generator_info.json')
+        self.scheduler_info = self.load_json('scheduler_info.json')
+
