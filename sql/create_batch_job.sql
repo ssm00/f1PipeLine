ddb9b810213228f18fb8e781f4087fec66cf8a21
ssm00 <titan0724@mju.ac.kr>
2024-11-25 23:09:00 +0900

    Update create_batch_job.sql

--- sql/create_batch_job.sql
+++ sql/create_batch_job.sql
@@ -1,11 +1,12 @@
 CREATE TABLE batch_jobs (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     job_name VARCHAR(100) NOT NULL,
-    status VARCHAR(20) NOT NULL,
-    start_time datetime,
-    end_time datetime,
-    duration decimal(10,3),
-    error_message text,
-    formatted_duration varchar(30),
-    INDEX strat_time (strat_time)
+    status VARCHAR(20) NOT NULL,  -- 'RUNNING', 'SUCCESS', 'FAILED'
+    start_time DATETIME NOT NULL,
+    end_time DATETIME,
+    duration int,
+    formatted_duration varchar(20),
+    error_message TEXT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
